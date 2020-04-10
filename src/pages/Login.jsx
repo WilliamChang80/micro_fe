8805648598eb8b8a9b0acad1969412619e90819d
WilliamChang80 <william_chang80@rocketmail.com>
2020-04-11 02:45:36 +0700

    Setup For Image And API

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Login extends Component {
-  state = {};
+  state = {
+    data: []
+  };
+
   render() {
-    return <div>Hello</div>;
+    const getImages = (imageName) => {
+      axios.get("https://api.unsplash.com/", {
+        client_id: process.env.API_KEY,
+        query: imageName
+      });
+    };
+
+    const renderData = async () => {
+      const data = await axios.get("http://localhost:8091/api/food");
+      const datas = data.data.data;
+      datas.map((x) => getImages(x.foodName));
+      return this.state.data.map((data) => (
+        <div>
+          <img src={data.imageName} />
+          {data.foodName}
+        </div>
+      ));
+    };
+    return <div>{renderData()}</div>;
   }
 }
 
